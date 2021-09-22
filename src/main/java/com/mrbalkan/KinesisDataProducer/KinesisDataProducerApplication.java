4527047309742d54e94117c9318422475a60bacf
Balkan <muratbal@3c22fbc0c0ec.ant.amazon.com>
2021-09-22 13:48:06 -0400

    initial commit

---
+++ src/main/java/com/mrbalkan/KinesisDataProducer/KinesisDataProducerApplication.java
@@ -0,0 +1,13 @@
+package com.mrbalkan.KinesisDataProducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KinesisDataProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KinesisDataProducerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mrbalkan/KinesisDataProducer/KinesisDataProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrbalkan.KinesisDataProducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KinesisDataProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
